fddcd1c13972b30c18c195385da657aa64e1b4be
Kurzacz, Kamil <kamil.kurzacz@intel.com>
2023-07-24 12:17:33 +0200

    Create install script for nvim configuration

---
+++ nvim/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Vim config
+mkdir -p ~/.config/nvim
+ln -sf ./.vimrc ~/.config/nvim/init.vim
+
+# Colors and syntax
+mkdir -p ~/.config/nvim/colors
+mkdir -p ~/.config/nvim/syntax
+
+ln -sf ./colors/asmox-gruvbox.vim ~/.config/nvim/colors/asmox-gruvbox.vim
+ln -sf ./syntax/python.vim ~/.config/nvim/syntax/python.vim
+
+# Install plugin system
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
