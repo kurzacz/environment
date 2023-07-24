371068168c6ddf6c9febfbf715ec4437392edf8d
Kurzacz, Kamil <kamil.kurzacz@intel.com>
2023-07-24 12:25:38 +0200

    Fix soft links target paths

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,14 +2,14 @@
 
 # Vim config
 mkdir -p ~/.config/nvim
-ln -sf ./.vimrc ~/.config/nvim/init.vim
+ln -sf $(pwd)/.vimrc ~/.config/nvim/init.vim
 
 # Colors and syntax
 mkdir -p ~/.config/nvim/colors
 mkdir -p ~/.config/nvim/syntax
 
-ln -sf ./colors/asmox-gruvbox.vim ~/.config/nvim/colors/asmox-gruvbox.vim
-ln -sf ./syntax/python.vim ~/.config/nvim/syntax/python.vim
+ln -sf $(pwd)/colors/asmox-gruvbox.vim ~/.config/nvim/colors/asmox-gruvbox.vim
+ln -sf $(pwd)/syntax/python.vim ~/.config/nvim/syntax/python.vim
 
 # Install plugin system
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
