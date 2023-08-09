285fcd445bdf149780ae45c4cfb8e929e4a5dc3f
Kurzacz, Kamil <kamil.kurzacz@intel.com>
2023-08-09 14:06:40 +0200

    Fix: allow nvim install script to be executed everywhere
    It's done by finding the absolute directory of install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+SCRIPT_SRC=$(realpath "${0}")
+SCRIPT_DIR=$(dirname "${SCRIPT_SRC}")
+
 # Vim config
 mkdir -p ~/.config/nvim
-ln -sf $(pwd)/.vimrc ~/.config/nvim/init.vim
+ln -sf "${SCRIPT_DIR}/.vimrc" ~/.config/nvim/init.vim
 
 # Colors and syntax
 mkdir -p ~/.config/nvim/colors
 mkdir -p ~/.config/nvim/syntax
 
-ln -sf $(pwd)/colors/asmox-gruvbox.vim ~/.config/nvim/colors/asmox-gruvbox.vim
-ln -sf $(pwd)/syntax/python.vim ~/.config/nvim/syntax/python.vim
+ln -sf "${SCRIPT_DIR}/colors/asmox-gruvbox.vim" ~/.config/nvim/colors/asmox-gruvbox.vim
+ln -sf "${SCRIPT_DIR}/syntax/python.vim" ~/.config/nvim/syntax/python.vim
 
 # Install plugin system
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
